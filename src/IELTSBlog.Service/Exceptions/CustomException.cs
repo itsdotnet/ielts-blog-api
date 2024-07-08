92b7da246c5ec7dc3d23768075cd6152f644c2b4
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-09 00:25:30 +0500

    Exceptions wrote

---
+++ src/IELTSBlog.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+namespace IELTSBlog.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public CustomException(int statusCode, string message) : base(message)
+    {
+        this.StatusCode = statusCode;
+    }
+    
+    public int StatusCode { get; set; }
+}
