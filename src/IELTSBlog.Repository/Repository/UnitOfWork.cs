8f11357e142a45b5455f4caf15e32206ee1b5cc3
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-08 23:35:30 +0500

    Repository wrote

---
+++ src/IELTSBlog.Repository/Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+using IELTSBlog.Domain.Entities;
+using IELTSBlog.Repository.Contexts;
+using IELTSBlog.Repository.IRepositories;
+
+namespace IELTSBlog.Repository.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _dbContext;
+
+    public UnitOfWork(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        
+        UserRepository = new Repository<User>(dbContext);
+        ArticleRepository = new Repository<Article>(dbContext);
+        CategoryRepository = new Repository<Category>(dbContext);   
+        CommentRepository = new Repository<Comment>(dbContext);
+    }
+    
+    public IRepository<User> UserRepository { get; }
+    public IRepository<Article> ArticleRepository { get; }
+    public IRepository<Category> CategoryRepository { get; }
+    public IRepository<Comment> CommentRepository { get; }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(true);
+    }
+    
+    public async Task<bool> SaveAsync()
+    {
+        var result = await _dbContext.SaveChangesAsync();
+        return result > 0;
+    }
+}
